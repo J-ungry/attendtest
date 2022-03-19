28fdec216a07bb463c01efd9cfa105f6365d4d7c
jungry <seojungrain@gmail.com>
2022-03-19 19:40:14 +0900

    html 생성

---
+++ src/main/java/com/attendtest/attendtest/AttendtestApplication.java
@@ -0,0 +1,13 @@
+package com.attendtest.attendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/attendtest/attendtest/AttendtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendtest.attendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
